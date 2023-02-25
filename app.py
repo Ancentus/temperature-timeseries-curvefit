aeb927543555d7773b11515dd08e7b2f7c9e378b
Ancentus <mancentus7@gmail.com>
2023-02-25 20:23:02 +0300

    Analyze monthly data per year

--- app.py
+++ app.py
@@ -25,19 +25,25 @@ def app():
     # Load the data from the CSV file
     date, temperature, date_num = load_data('monthly_csv.csv')
 
+    # Add a slider for the year
+    year = st.slider('Select a year:', 1880, 2016, 2000)
+    
+    # Get the indices of the data for the selected year
+    year_indices = np.where(np.array([d.year for d in date]) == year)[0]
+    
     # Fit the function to the data using curve_fit
-    popt, pcov = curve_fit(f, date_num, temperature)
+    popt, pcov = curve_fit(f, date_num[year_indices], temperature[year_indices])
 
     # Display the optimized parameters
     st.write('Optimized Parameters:')
-    st.write(f'a = {popt[0]} +/- {pcov[0,0]**.5}')
-    st.write(f'b = {popt[1]} +/- {pcov[1,1]**.5}')
-    st.write(f'c = {popt[2]} +/- {pcov[2,2]**.5}')
+    st.write(f'a = {popt[0]} +/- {pcov[0,0]**.5:.3f}')
+    st.write(f'b = {popt[1]} +/- {pcov[1,1]**.5:.3f}')
+    st.write(f'c = {popt[2]} +/- {pcov[2,2]**.5:.3f}')
 
-    # Plot the data and the best fit model
+    # Plot the data and the best fit model for the selected year
     fig, ax = plt.subplots()
-    ax.plot(date, temperature, 'b.', label='Data')
-    ax.plot(date, f(date_num, *popt), 'r-', label='Best fit model')
+    ax.plot(date[year_indices], temperature[year_indices], 'b.', label='Data')
+    ax.plot(date[year_indices], f(date_num[year_indices], *popt), 'r-', label='Best fit model')
     ax.set_xlabel('Date')
     ax.set_ylabel('Mean Temperature Anomaly (C)')
     ax.legend()
