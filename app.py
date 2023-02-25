f485e684dc8a40dd444fc06b2321ba7fc43ac869
Ancentus <mancentus7@gmail.com>
2023-02-25 05:07:50 +0300

    Fit function to data

--- app.py
+++ app.py
@@ -12,3 +12,28 @@ date, temperature = np.loadtxt('monthly_csv.csv', delimiter=',', skiprows=1,
 # Convert date strings to numerical values representing days since the first date
 date_num = np.array([(d - date[0]).days for d in date])
 
+# Define the function f(t) = a*cos(2*pi*t + b) + c
+def f(t, a, b, c):
+    return a * np.cos(2 * np.pi * t + b) + c
+
+# Fit the function to the data using curve_fit
+popt, pcov = curve_fit(f, date_num, temperature)
+
+# Display the optimized parameters
+print(popt)
+print("a= ", popt[0], "+/-", pcov[0,0]**.5)
+print("b= ", popt[1], "+/-", pcov[1,1]**.5)
+print("c= ", popt[2], "+/-", pcov[2,2]**.5)
+
+
+# Plot the data and the best fit model
+plt.plot(date, temperature, 'b.', label='Data')
+plt.plot(date, f(date_num, *popt), 'r-', label='Best fit model')
+
+# Add axis labels and a legend
+plt.xlabel('Date')
+plt.ylabel(' Mean Temperature Anomaly (C)')
+plt.legend()
+
+# Display the plot
+plt.show()
