6718407451633df5bb4f34f7d6efc764b1be3eee
Ancentus <mancentus7@gmail.com>
2023-02-25 05:06:45 +0300

    Convert dates to days

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ date, temperature = np.loadtxt('monthly_csv.csv', delimiter=',', skiprows=1,
                   dtype=[('date', object), ('temperature', float)],
                   converters={1: lambda d: datetime.strptime(d.decode(), '%Y-%m-%d')})
 
+# Convert date strings to numerical values representing days since the first date
+date_num = np.array([(d - date[0]).days for d in date])
+
