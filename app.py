694d5f2850c4d8a3370251f88f7d6f5a3b6cd9ac
Ancentus <mancentus7@gmail.com>
2023-02-25 05:04:25 +0300

    Load data to numpy array

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from datetime import datetime
+
+# Load the data from the CSV file into a numpy array
+date, temperature = np.loadtxt('monthly_csv.csv', delimiter=',', skiprows=1,
+                  usecols=(1, 2), unpack=True, 
+                  dtype=[('date', object), ('temperature', float)],
+                  converters={1: lambda d: datetime.strptime(d.decode(), '%Y-%m-%d')})
+
