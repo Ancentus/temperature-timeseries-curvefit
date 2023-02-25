1313d84229f351a6f6e87bf97b631e4c49898080
Ancentus <mancentus7@gmail.com>
2023-02-25 19:40:37 +0300

    Convert to streamlit app

--- app.py
+++ app.py
@@ -2,38 +2,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from datetime import datetime
+import streamlit as st
 
-# Load the data from the CSV file into a numpy array
-date, temperature = np.loadtxt('monthly_csv.csv', delimiter=',', skiprows=1,
+# Define a function to load the data from the CSV file
+def load_data(filename):
+    date, temperature = np.loadtxt(filename, delimiter=',', skiprows=1,
                   usecols=(1, 2), unpack=True, 
                   dtype=[('date', object), ('temperature', float)],
                   converters={1: lambda d: datetime.strptime(d.decode(), '%Y-%m-%d')})
-
-# Convert date strings to numerical values representing days since the first date
-date_num = np.array([(d - date[0]).days for d in date])
+    date_num = np.array([(d - date[0]).days for d in date])
+    return date, temperature, date_num
 
 # Define the function f(t) = a*cost(2*pi*t + b) + c
 def f(t, a, b, c):
     return a * np.cos(t) * (2 * np.pi * t + b) + c
 
-# Fit the function to the data using curve_fit
-popt, pcov = curve_fit(f, date_num, temperature)
+# Define the Streamlit app
+def app():
+    # Set the title of the app
+    st.title('Temperature Data Analysis')
 
-# Display the optimized parameters
-print(popt)
-print("a= ", popt[0], "+/-", pcov[0,0]**.5)
-print("b= ", popt[1], "+/-", pcov[1,1]**.5)
-print("c= ", popt[2], "+/-", pcov[2,2]**.5)
+    # Load the data from the CSV file
+    date, temperature, date_num = load_data('monthly_csv.csv')
 
+    # Fit the function to the data using curve_fit
+    popt, pcov = curve_fit(f, date_num, temperature)
 
-# Plot the data and the best fit model
-plt.plot(date, temperature, 'b.', label='Data')
-plt.plot(date, f(date_num, *popt), 'r-', label='Best fit model')
+    # Display the optimized parameters
+    st.write('Optimized Parameters:')
+    st.write(f'a = {popt[0]} +/- {pcov[0,0]**.5}')
+    st.write(f'b = {popt[1]} +/- {pcov[1,1]**.5}')
+    st.write(f'c = {popt[2]} +/- {pcov[2,2]**.5}')
 
-# Add axis labels and a legend
-plt.xlabel('Date')
-plt.ylabel(' Mean Temperature Anomaly (C)')
-plt.legend()
+    # Plot the data and the best fit model
+    fig, ax = plt.subplots()
+    ax.plot(date, temperature, 'b.', label='Data')
+    ax.plot(date, f(date_num, *popt), 'r-', label='Best fit model')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Mean Temperature Anomaly (C)')
+    ax.legend()
+    st.write(fig)
 
-# Display the plot
-plt.show()
+# Run the app
+if __name__ == '__main__':
+    app()
+
